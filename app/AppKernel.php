f0e7b06db386403bc77b6b017e091b3f0107d20b
S7tH <aurelien.theriot@gmail.com>
2018-01-29 18:36:42 +0100

    remove ckfinderBundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -18,7 +18,6 @@ class AppKernel extends Kernel
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new AppBundle\AppBundle(),
             new FOS\UserBundle\FOSUserBundle(),
-            new CKSource\Bundle\CKFinderBundle\CKSourceCKFinderBundle(),
             new Http\HttplugBundle\HttplugBundle(),
             new HWI\Bundle\OAuthBundle\HWIOAuthBundle(),
         ];
